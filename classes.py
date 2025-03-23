ce8a3dcea45650b1b52e982eaeb9c26ba47d4622
Nesha-macherla <nesha.macherla@gmail.com>
2025-03-24 01:36:59 +0530

    Update classes.py

--- classes.py
+++ classes.py
@@ -19,8 +19,16 @@ from gtts import gTTS
 import matplotlib
 matplotlib.use('Agg')  
 
-nltk.download('vader_lexicon', quiet=True)
-nltk.download('stopwords', quiet=True)
+
+# Set a writable directory for NLTK downloads
+nltk_data_dir = "/app/nltk_data"
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+# Download necessary NLTK resources
+nltk.download('vader_lexicon', download_dir=nltk_data_dir, quiet=True)
+nltk.download('punkt', download_dir=nltk_data_dir, quiet=True)
+nltk.download('stopwords', download_dir=nltk_data_dir, quiet=True)
 
 
 stop_words = set(stopwords.words('english'))
