343599e6129cadded4ae6c7dd6fa51045cd46640
Nesha-macherla <nesha.macherla@gmail.com>
2025-03-24 00:29:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ import signal
 # Function to start the Flask API server
 def start_api_server():
     from api import app
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=False, threaded=True)
 
 # Function to start the Streamlit frontend
 def start_streamlit():
-    subprocess.run(["streamlit", "run", "app_frontend.py", "--server.port", "8501", "--server.address", "0.0.0.0"])
+    subprocess.run(["streamlit", "run", "app_frontend.py", "--server.port", "8501", "--server.address", "0.0.0.0","--server.headless", "true",
+        "--browser.serverAddress", "0.0.0.0",
+        "--browser.gatherUsageStats", "false"])
 
 if __name__ == "__main__":
     # Start Flask API in a separate thread
@@ -20,7 +22,7 @@ if __name__ == "__main__":
     api_thread.start()
     
     # Give the API a moment to start
-    time.sleep(2)
+    time.sleep(5)
     
     # Start Streamlit in the main thread
     start_streamlit()
